4c426de4ccb78765eaa02f80d09a885e83e1c0ed
LuscaP3 <149536739+LuscaP3@users.noreply.github.com>
2025-09-12 23:02:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { defineConfig } from 'vite';
 
-export default{
+export default defineConfig({
   plugins: [wasm(), topLevelAwait()],
   base: '/MyEngine/',
-};
+});
